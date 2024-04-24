33bce1a593e7617a35b8586c920e8d9307a16e1f
Alexey K <a.khrameev@it.ecommpay.com>
2024-04-24 09:06:52 +0300

    fix descriptions and metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,4 @@
 ## [1.0.0] - First release
 ## [1.0.1] - Some platform fixes
+## [1.0.2] - Fix metadata and description
+
--- lib/models/ecmp_payment_info.dart
+++ lib/models/ecmp_payment_info.dart
@@ -1,18 +1,21 @@
 import 'package:json_annotation/json_annotation.dart';
 
-
 part 'ecmp_payment_info.g.dart';
 
 @JsonSerializable()
 class EcmpPaymentInfo {
   /// project (merchant) ID
   final int projectId;
+
   /// must be unique within the project
   final String paymentId;
+
   /// payment amount in minor currency units
   final int paymentAmount;
+
   /// payment currency code according to ISO-4217 alpha-3
   final String paymentCurrency;
+
   /// payment description (this parameter is available not only to the merchant, but also to the customer; if paymentDescription is specified in the request, it is visible to the customer in the payment form (in the dialog box containing information about the payment); if this parameter is not specified in the request, it is not visible to the customer)
   final String? paymentDescription;
   final String? customerId;
@@ -20,8 +23,10 @@ class EcmpPaymentInfo {
   final String? token;
   final String? languageCode;
   final String? receiptData;
+
   /// hiding or displaying saved payment instruments in the payment form
   final bool? hideSavedWallets;
+
   /// the identifier of the payment method which is opened to the customer without an option for the customer to select another payment method. The list of codes is provided in the IDs of payment methods supported on Payment Page section
   final String? forcePaymentMethod;
   String? signature;
